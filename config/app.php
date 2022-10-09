ba5b857a7d7510a9083b20a6bdec2afcf17cab32
mohammedam33n <104563781+mohammedam33n@users.noreply.github.com>
2022-10-09 16:00:05 +0200

    int datatable

--- config/app.php
+++ config/app.php
@@ -186,6 +186,7 @@ return [
          * Package Service Providers...
          */
         RealRashid\SweetAlert\SweetAlertServiceProvider::class,
+        Yajra\DataTables\DataTablesServiceProvider::class,
 
         /*
          * Application Service Providers...
